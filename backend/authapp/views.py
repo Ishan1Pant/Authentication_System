a0d77e1d7a61d0e38d3500e3d650b8d8cc5a3264
Ishan1Pant <ishanpant104@gmail.com>
2024-01-25 19:35:04 +0530

    Applied JWT

--- backend/authapp/views.py
+++ backend/authapp/views.py
@@ -4,17 +4,28 @@ from rest_framework.views import APIView
 from .serializers import MyUserSerializer,LoginSerializer
 from django.contrib.auth import authenticate
 from .renderers import UserRenderer
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+#Create Tokens
+def get_tokens(user):
+    refresh =RefreshToken.for_user(user)
+    return{
+        'refresh':str(refresh),
+        'access':str(refresh.access_token),
+    }
+
+
 
 class userRegisterView(APIView):
     renderer_classes=[UserRenderer]
     def post(self,request):
         serializer=MyUserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response({'msg':'Success'},status=status.HTTP_201_CREATED)
+            user=serializer.save()
+            token=get_tokens(user)
+            return Response({'token':token,'msg':'Success'},status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     
 class userLoginView(APIView):
@@ -26,7 +37,8 @@ class userLoginView(APIView):
             password=serializer.data.get('password')
             user=authenticate(email=email,password=password)
             if user is not None:
-                return Response({"msg":"login Successfull"},status=status.HTTP_200_OK)
+                token=get_tokens(user)
+                return Response({"token":token,"msg":"login Successfull"},status=status.HTTP_200_OK)
             else:
                 return Response({"Errors":{"Non_field_errors":["Invalid Useraname of Password"]}},status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
